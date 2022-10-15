3337ccaca5f2d3cd34918ca7a79ea2c758cb1b72
Patrick Son <pmacedo10_@hotmail.com>
2022-10-15 18:01:56 -0300

    Adicionado elemento da aplicação

--- lib/styles/colours.dart
+++ lib/styles/colours.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 //THEME ONE
-const principal = Color.fromARGB(255, 91, 252, 69);
+const principal = Color.fromARGB(255, 41, 250, 93);
 const primary = Color(0xff2ca58d);
 const secondary = Color(0xff0a2342);
 const ternary = Color(0xff84bc9c);
